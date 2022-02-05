d62472e4cd4609a3a9dc1ec8ccda28bdec2295aa
lnicepei <dmitrii.shaidorov@gmail.com>
2022-02-05 12:13:33 +0300

    soft transition of color and not working buttons of change color, reset, and reset size

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
-let n = parseInt(prompt("enter number of row & columns(less than a 100)"));
-document.getElementById('container').style.width = n * 27 + 'px';
-document.getElementById('container').style.height = n * 27 + 'px';
-
-if(n < 100){
+let n = 0;
+hotLoad();
+function hotLoad(){
+    n = parseInt(prompt("enter number of row & columns(less than a 32)"));
+    document.getElementById('container').style.width = n * 25 + 'px';
+    document.getElementById('container').style.height = n * 25 + 'px';
+    
+    if(n < 100){
+        for(let i = 0; i < n; i++){
+            for(let k = 0; k < n; k++){
+                const div = document.createElement('div');
+                div.setAttribute("class", "square");
+                document.getElementById('container').appendChild(div);
+                div.addEventListener("mouseover", function adder() {
+                    div.classList.toggle('hover');
+                    this.removeEventListener('mouseover', adder, false);
+                }, false);
+            }
+            
+        }
+        
+        document.getElementById('resetbutton').addEventListener('click', reseter(), false);
+    }else{
+        alert("enter a number less than a 100");
+        // document.location.reload();
+    }
+}
+function reseter(){
     for(let i = 0; i < n; i++){
         for(let k = 0; k < n; k++){
-            const div = document.createElement('div');
-            div.setAttribute("class", "square");
-            document.getElementById('container').appendChild(div);
-            div.addEventListener("mouseover", function adder() {
-                div.classList.toggle('hover');
-                this.removeEventListener('mouseover', adder, false);
-              }, false);
+            
+            // document.getElementById('container').removeChild('square');
+            
         }
-    
     }
-
-}else{
-    alert("enter a number less than a 100");
-    // document.location.reload();
 }
-
-document.getElementById('resetbutton').addEventListener('click', function (){
-    n = parseInt(prompt("enter number of row & columns(less than a 100)"));
-    
-}, false);
-  
+function colorSelected (element) {
+    document.body.style.background = element.value
+}

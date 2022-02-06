0cd36567bcf0f5b0dd2e1d879102303f7d8952ff
lnicepei <dmitrii.shaidorov@gmail.com>
2022-02-06 13:12:31 +0300

    set04ka(werid but exists

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 let n = 0;
 etch();
 function etch(){
-    n = parseInt(prompt("enter number of row & columns(32 or less)"));
+    do{
+        n = parseInt(prompt("enter number of row & columns(32 or less)"));
+        if(n > 32){alert("enter number 32 or less");}
+    }while(n > 32)
+    
     document.getElementById('container').style.width = n * 25 + 'px';
     document.getElementById('container').style.height = n * 25 + 'px';
-    
-    if(n < 33){
-        for(let i = 0; i < n; i++){
-            for(let k = 0; k < n; k++){
-                const div = document.createElement('div');
-                div.setAttribute("class", "square");
-                document.getElementById('container').appendChild(div);
-                div.addEventListener("mouseover", function adder() {
-                    div.classList.toggle('hover');
-                    this.removeEventListener('mouseover', adder, false);
-                }, false);
-            }
+    for(let i = 0; i < n; i++){
+        for(let k = 0; k < n; k++){
+            const div = document.createElement('div');
+            div.setAttribute("class", "square");
+            document.getElementById('container').appendChild(div);
+            div.addEventListener("mouseover", function adder() {
+                div.classList.toggle('hover');
+                this.removeEventListener('mouseover', adder, false);
+            }, false);
         }
-        
-        document.getElementById('resetbutton').addEventListener('click', reseter, false);
-        document.getElementById('resizer').addEventListener('click', resizer, false);
-    }else{
-        alert("enter number 32 or less");
     }
+    
+    document.getElementById('resetbutton').addEventListener('click', reseter, false);
+    document.getElementById('resizer').addEventListener('click', resizer, false);
 }
+   
 function reseter(){            
     const divs = document.querySelectorAll(".square");
     divs.forEach(div => { divs.forEach(d => document.getElementById('container').removeChild(d));
@@ -44,7 +44,10 @@ function reseter(){
 function resizer(){
     const divs = document.querySelectorAll(".square");
     divs.forEach(div => { divs.forEach(d => document.getElementById('container').removeChild(d));
-    n = parseInt(prompt("enter number of row & columns(32 or less)"));
+    do{
+        n = parseInt(prompt("enter number of row & columns(32 or less)"));
+        if(n > 32){alert("enter number 32 or less");}
+    }while(n > 32)
     document.getElementById('container').style.width = n * 25 + 'px';
     document.getElementById('container').style.height = n * 25 + 'px';
     for(let i = 0; i < n; i++){

7f8ef7278a7853ade42a4294e01d2ccb0292db36
lnicepei <dmitrii.shaidorov@gmail.com>
2022-02-06 12:49:07 +0300

    add working resize button

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let n = 0;
-hotLoad();
-function hotLoad(){
-    n = parseInt(prompt("enter number of row & columns(less than a 32)"));
+etch();
+function etch(){
+    n = parseInt(prompt("enter number of row & columns(32 or less)"));
     document.getElementById('container').style.width = n * 25 + 'px';
     document.getElementById('container').style.height = n * 25 + 'px';
     
-    if(n < 100){
+    if(n < 33){
         for(let i = 0; i < n; i++){
             for(let k = 0; k < n; k++){
                 const div = document.createElement('div');
@@ -16,13 +16,12 @@ function hotLoad(){
                     this.removeEventListener('mouseover', adder, false);
                 }, false);
             }
-            
         }
         
         document.getElementById('resetbutton').addEventListener('click', reseter, false);
+        document.getElementById('resizer').addEventListener('click', resizer, false);
     }else{
-        alert("enter a number less than a 100");
-        // document.location.reload();
+        alert("enter number 32 or less");
     }
 }
 function reseter(){            
@@ -41,6 +40,26 @@ function reseter(){
             
         }
 });         
+}
+function resizer(){
+    const divs = document.querySelectorAll(".square");
+    divs.forEach(div => { divs.forEach(d => document.getElementById('container').removeChild(d));
+    n = parseInt(prompt("enter number of row & columns(32 or less)"));
+    document.getElementById('container').style.width = n * 25 + 'px';
+    document.getElementById('container').style.height = n * 25 + 'px';
+    for(let i = 0; i < n; i++){
+            for(let k = 0; k < n; k++){
+                const div = document.createElement('div');
+                div.setAttribute("class", "square");
+                document.getElementById('container').appendChild(div);
+                div.addEventListener("mouseover", function adder() {
+                    div.classList.toggle('hover');
+                    this.removeEventListener('mouseover', adder, false);
+                }, false);
+            }
+            
+        }
+}); 
 }
 function colorSelected (element) {
     document.getElementsByClassName('#container > .square > .hover').style.backgroundColor = element.value

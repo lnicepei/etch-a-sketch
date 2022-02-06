0e352b03b6546c5a763b8acbccbf61a4561ff8c8
lnicepei <dmitrii.shaidorov@gmail.com>
2022-02-06 12:43:18 +0300

    add reset button

--- script.js
+++ script.js
@@ -26,11 +26,22 @@ function hotLoad(){
     }
 }
 function reseter(){            
-    const divs = document.querySelectorAll(".square.hover");
-    divs.forEach(div => { divs.forEach(d => d.classList.remove("hover"));
-    d.classList.add("hoverclear");
+    const divs = document.querySelectorAll(".square");
+    divs.forEach(div => { divs.forEach(d => document.getElementById('container').removeChild(d));
+    for(let i = 0; i < n; i++){
+            for(let k = 0; k < n; k++){
+                const div = document.createElement('div');
+                div.setAttribute("class", "square");
+                document.getElementById('container').appendChild(div);
+                div.addEventListener("mouseover", function adder() {
+                    div.classList.toggle('hover');
+                    this.removeEventListener('mouseover', adder, false);
+                }, false);
+            }
+            
+        }
 });         
 }
 function colorSelected (element) {
-    document.getElementsByClassName('container square hover').style.backgroundColor = element.value
+    document.getElementsByClassName('#container > .square > .hover').style.backgroundColor = element.value
 }
